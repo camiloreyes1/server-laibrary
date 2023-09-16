8f3a0289056d09861ba2b8ea20f99bec069069b3
camiloreyes1 <jreye236@fiu.edu>
2023-09-16 01:18:06 -0400

    first commit

--- models/User.js
+++ models/User.js
@@ -1,5 +1,31 @@
 const {Schema, model, Types} = require("mongoose")
 
-const UserSchema = new Schema(
+const userSchema = new Schema(
+    {
+        fullName: String,
+
+        username: {
+        type: String,
+        unique: true,
+        required: true
+        },
+
+        email: {
+        type: String,
+        unique: true,
+        required: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        }
+
+    },
     
+    {
+        timeseries: true
+    }
 )
+
+module.exports = model("User", userSchema);
