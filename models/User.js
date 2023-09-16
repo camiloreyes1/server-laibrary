1ad9f427b80ebd55aaa089b5b9dfc97637824f01
camiloreyes1 <jreye236@fiu.edu>
2023-09-16 01:23:46 -0400

    first commit

--- models/User.js
+++ models/User.js
@@ -17,8 +17,8 @@ const userSchema = new Schema(
         },
 
         password: {
-            type: String,
-            required: true
+        type: String,
+        required: true
         }
 
     },
