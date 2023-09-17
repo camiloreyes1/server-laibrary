b1f004e1fcef6965a8d950ff0d05b85db605ff69
camiloreyes1 <jreye236@fiu.edu>
2023-09-17 02:20:24 -0400

    profile commit

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,15 @@ const isAuthenticated = require('../middleware/isAuthenticated');
 const User = require('../models/User');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 router.post('/edit/:userId', isAuthenticated, (req,res,next) => {
 
-  const { occupation } = req.body
+  const { occupation, fullName } = req.body
 
   User.findByIdAndUpdate
   (
     req.params.userId, 
     {
+      fullName,
       occupation
     },
     {new: true}
@@ -30,6 +26,14 @@ router.post('/edit/:userId', isAuthenticated, (req,res,next) => {
     next(err)
 })
 
+});
+
+router.get('/profile/:id', isAuthenticated, (req,res,next) => {
+  User.findById(req.user._id)
+  .then((foundUser) => {
+    console.log("FoundUser ===>",foundUser)
+    res.json(foundUser)
+  })
 })
 
 module.exports = router;
